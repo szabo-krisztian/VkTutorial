936408e6c034b77c8dc355b60bd811bb9ced4287
Krisztian Szabo <szabo.krisztian.hun@gmail.com>
2024-08-03 22:05:32 +0200

    Started integrating default renderpass into app_base

--- code/bootstrap/app_base.hpp
+++ code/bootstrap/app_base.hpp
@@ -37,27 +37,45 @@ protected:
     const int WINDOW_WIDTH = 1920;
     const int WINDOW_HEIGHT = 1080;
 
-    VkInstance               instance;
-    VkDebugUtilsMessengerEXT debugMessenger;
-    GLFWwindow               *window;
-    VkSurfaceKHR             surface;
-    PhysicalDevice           physicalDevice;
-    Device                   device;
-    Swapchain                swapchain;
-    InputManager             *inputManager;
-    Camera                   camera;
-    Timer                    timer;
-    bool                     isAppRunning = false;
+    VkInstance                 instance;
+    VkDebugUtilsMessengerEXT   debugMessenger;
+    GLFWwindow                 *window;
+    VkSurfaceKHR               surface;
+    PhysicalDevice             physicalDevice;
+    Device                     device;
+    Swapchain                  swapchain;
+    VkRenderPass               renderPass;
+    std::vector<VkFramebuffer> framebuffers;
+    InputManager               *inputManager;
+    Camera                     camera;
+    Timer                      timer;
+    bool                       isAppRunning = false;
 
     virtual void Update() = 0;
 
 private:
+    struct DepthBuffer
+    {
+        VkImage        depthImage;
+        VkDeviceMemory depthImageMemory;
+        VkImageView    depthImageView;
+    } _depthBuffer;
+
     DeletionQueue _deletionQueue;
 
     void Init();
     void InitGLFW();
     void InitVulkan();
     void InitSwapchain();
+
+    void        InitDepthBuffer();
+    VkFormat    FindDepthFormat();
+    VkFormat    FindSupportedFormat(const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
+    void        CreateImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+    VkImageView CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+
+    void InitRenderPass();
+    void InitFramebuffers();
     void InitInputManager();
 };
 
